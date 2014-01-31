e6f945b4cdbc86b862f8ec39910802cd4126e443
Shravan Kumar <shravan@shravan-HP-Pavilion-dv6-Notebook-PC.(none)>
2014-01-31 11:40:27 +0530

    Working on opencv

---
+++ cpp/main.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <string>
+#include <sys/stat.h>
+#include <iterator>
+#include <boost/filesystem.hpp>
+
+
+#ifndef CSV_NAME
+#define CSV_NAME images.csv
+#endif
+
+#ifndef CSV_PATH 
+#define CSV_PATH "./" 
+#endif
+
+#ifndef IMAGE_DIRECTORY_PATH
+#define IMAGE_DIRECTORY_PATH "./images/"
+#endif
+
+struct s_exception {
+	
+	private:
+	string msg;
+
+	public:
+	
+	void set_msg(String e) {
+		msg = e;
+	}
+
+	string get_msg() {
+		return msg;
+	}
+
+} e;
+
+using namespace std;
+using namespace boost::filesystem;
+
+int main(int argc, char *argv[]) {
+	if(argc < 3) {
+		cerr << "Usage: face_recognizer <action_parameter> <value_parameter_list>\n";
+		return 1;
+	}
+	string action = argv[1], value = argv[2];
+	if(action == "add") {
+		/* Need Username and the current image location */
+		if(argc != 4) {
+			e.set_msg(new string("Invalid Arguments. Usage: face_recognizer add <username> <image_location>"));
+			throw e;
+		}
+		string username = argv[2], img = argv[3];
+		path p(IMAGE_DIRECTORY_PATH + username);
+		if( ! exists(p) ) {
+		}
+	}
+	else if(action == "detect") {
+		/* Need Username and the test image */
+		if(argc != 4) {
+			e.set_msg(new string("Invalid Arguments. Usage: face_recognizer detect <username> <test_image_location>"));
+			throw e;
+		}
+	}
+	else {
+		;
+	}
+	catch (s_exception t) {
+		cerr << t.get_msg() << endl;
+		return 1;
+	}
+	return 0;
+}
